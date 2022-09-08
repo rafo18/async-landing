feca449b45747de0a5c3ff2172f5129120cb42e9
Rafael Barreto <rafaelbarreto10@hotmail.com>
2022-09-07 21:34:20 -0400

    Updates

---
+++ assets/main.js
@@ -0,0 +1,47 @@
+const API ='https://youtube-v31.p.rapidapi.com/search?channelId=UCynseHbeuQrStXXepsrzOdQ&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e6e7e6a24cmshdfb0a09524ea3a2p169508jsnfc0a85e1dbee',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData (urlApi) {
+    const response = await fetch(urlApi , options);
+    const data = await response.json();
+    return data;
+}
+
+(async() => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+            title
+          </h3>
+        </div>
+      </div>
+        `).slice(0,4).join('')} 
+        `; 
+        content.innerHTML = view;
+    } catch {
+        console.log(error);
+
+    }
+})();
+
+
